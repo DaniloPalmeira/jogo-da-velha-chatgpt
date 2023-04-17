0bf53bf2fe35da24b3c0186a0f041d24d9aaa7d3
DaniloPalmeira <srdanlino@icloud.com>
2023-04-17 05:02:51 -0300

    Game rewritten

--- script.js
+++ script.js
@@ -1,112 +1,131 @@
-// Variáveis globais
-var currentPlayer = "X";
-var gameEnded = false;
-var playerXScore = 0;
-var playerOScore = 0;
+var currentPlayer = 1;
+var player1Score = 0;
+var player2Score = 0;
+var gameOver = false;
+var winningCombinations = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[2, 4, 6],
+];
 
-// Elementos do DOM
-var cells = document.getElementsByClassName("cell");
-var resultModal = document.getElementById("result-modal");
-var resultMessage = document.getElementById("result-message");
-var restartButton = document.getElementById("restart-button");
-var resetButton = document.getElementById("reset-button");
-var playerXScoreElement = document.getElementById("player-x-score");
-var playerOScoreElement = document.getElementById("player-o-score");
+$(document).ready(function () {
+	$(".cell").on("click", function () {
+		if (!gameOver && $(this).text() === "") {
+			var cellId = $(this).attr("id");
+			var cellIndex = parseInt(cellId.replace("cell-", ""));
 
-// Função para reiniciar o jogo
-function restartGame() {
-	// Limpar o tabuleiro
-	for (var i = 0; i < cells.length; i++) {
-		cells[i].innerText = "";
-	}
-	// Resetar as variáveis
-	currentPlayer = "X";
-	gameEnded = false;
-}
-
-// Função para verificar se houve um vencedor
-function checkWinner() {
-	var winningCombinations = [
-		[0, 1, 2],
-		[3, 4, 5],
-		[6, 7, 8], // linhas
-		[0, 3, 6],
-		[1, 4, 7],
-		[2, 5, 8], // colunas
-		[0, 4, 8],
-		[2, 4, 6], // diagonais
-	];
-
-	for (var i = 0; i < winningCombinations.length; i++) {
-		var [a, b, c] = winningCombinations[i];
-		if (
-			cells[a].innerText === cells[b].innerText &&
-			cells[a].innerText === cells[c].innerText &&
-			cells[a].innerText !== ""
-		) {
-			return cells[a].innerText;
-		}
-	}
-
-	return null;
-}
-
-// Função para verificar se houve empate
-function checkDraw() {
-	for (var i = 0; i < cells.length; i++) {
-		if (cells[i].innerText === "") {
-			return false;
-		}
-	}
-	return true;
-}
-
-// Função para exibir o resultado do jogo
-function showResult(result) {
-	if (result === "X") {
-		resultMessage.innerText = "Jogador X venceu!";
-		playerXScore++;
-	} else if (result === "O") {
-		resultMessage.innerText = "Jogador O venceu!";
-		playerOScore++;
-	} else {
-		resultMessage.innerText = "Empate!";
-	}
-
-	playerXScoreElement.innerText = "Jogador X: " + playerXScore;
-	playerOScoreElement.innerText = "Jogador O: " + playerOScore;
-
-	resultModal.style.display = "block";
-	gameEnded = true;
-}
-
-// Função para reiniciar o jogo quando o botão "Jogar novamente" é clicado
-restartButton.addEventListener("click", function () {
-	resultModal.style.display = "none";
-	restartGame();
-});
-
-// Função para reiniciar o placar quando o botão "Reiniciar Placar" é clicado
-resetButton.addEventListener("click", function () {
-	playerXScore = 0;
-	playerOScore = 0;
-	playerXScoreElement.innerText = "Jogador X: 0";
-	playerOScoreElement.innerText = "Jogador O: 0";
-});
-
-// Loop para adicionar eventos de clique em cada célula do tabuleiro
-for (var i = 0; i < cells.length; i++) {
-	cells[i].addEventListener("click", function () {
-		if (this.innerText === "" && !gameEnded) {
-			this.innerText = currentPlayer;
-			var winner = checkWinner();
-			if (winner) {
-				showResult(winner);
-			} else if (checkDraw()) {
-				showResult(null);
+			if (currentPlayer === 1) {
+				$(this).text("X").css("color", "#FFD700");
 			} else {
-				currentPlayer = currentPlayer === "X" ? "O" : "X";
+				$(this).text("O").css("color", "#00FFFF");
+			}
+
+			var winner = checkWin();
+			if (winner !== 0) {
+				gameOver = true;
+				updateStatus("Jogador " + winner + " venceu!");
+				updateScore(winner);
+				playWinEffect();
+			} else if (checkDraw()) {
+				gameOver = true;
+				updateStatus("Empate!");
+				playDrawEffect();
+			} else {
+				switchPlayer();
 			}
 		}
 	});
-}
+
+	$("#reset-score").on("click", function () {
+		player1Score = 0;
+		player2Score = 0;
+		updateScoreboard();
+	});
+
+	$("#reset-game").on("click", function () {
+		// Limpa o texto das células do jogo
+		$(".cell").text("");
+		// Redefine a variável gameOver para false
+		gameOver = false;
+		// Atualiza o status do jogo para o jogador 1
+		updateStatus(`Vez do Jogador ${currentPlayer}`);
+		// Remove a cor de fundo das células
+		$(".cell").css("background-color", "");
+	});
+
+	function checkWin() {
+		for (var i = 0; i < winningCombinations.length; i++) {
+			var combination = winningCombinations[i];
+			if (
+				$("#cell-" + combination[0]).text() !== "" &&
+				$("#cell-" + combination[0]).text() ===
+					$("#cell-" + combination[1]).text() &&
+				$("#cell-" + combination[0]).text() ===
+					$("#cell-" + combination[2]).text()
+			) {
+				return currentPlayer;
+			}
+		}
+		return 0;
+	}
+
+	function checkDraw() {
+		var draw = true;
+		$(".cell").each(function () {
+			if ($(this).text() === "") {
+				draw = false;
+				return false;
+			}
+		});
+		return draw;
+	}
+
+	function switchPlayer() {
+		currentPlayer = currentPlayer === 1 ? 2 : 1;
+		updateStatus("Vez do Jogador " + currentPlayer);
+	}
+
+	function updateStatus(message) {
+		$("#status").text(message);
+	}
+
+	function updateScore(player) {
+		if (player === 1) {
+			player1Score++;
+		} else {
+			player2Score++;
+		}
+		updateScoreboard();
+	}
+
+	function updateScoreboard() {
+		$("#score-player1").text("Jogador 1: " + player1Score);
+		$("#score-player2").text("Jogador 2: " + player2Score);
+	}
+
+	function playWinEffect() {
+		$(".cell").css("background-color", "#222");
+		var winningCombination = winningCombinations.filter(function (combination) {
+			return (
+				$("#cell-" + combination[0]).text() !== "" &&
+				$("#cell-" + combination[0]).text() ===
+					$("#cell-" + combination[1]).text() &&
+				$("#cell-" + combination[0]).text() ===
+					$("#cell-" + combination[2]).text()
+			);
+		});
+		winningCombination = winningCombination[0];
+		$("#cell-" + winningCombination[0]).css("background-color", "#32CD32");
+		$("#cell-" + winningCombination[1]).css("background-color", "#32CD32");
+		$("#cell-" + winningCombination[2]).css("background-color", "#32CD32");
+	}
+
+	function playDrawEffect() {
+		$(".cell").css("background-color", "#FF4500");
+	}
+});
